c3cd02ee3e0e46d6f56e10854fe1796dc4f0b140
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-09-29 19:49:05 +0300

    app-navbar-wip-files

--- king-app/src/components/Navbar.js
+++ king-app/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 function Navbar() {
     return (
@@ -6,7 +7,7 @@ function Navbar() {
        <nav className="navbar">
            <div className="navbar-container">
              <Link to="/"className="navbar-logo">
-                 BETTIORIUM
+                 BETTIORIUM <i className="fas fa-crown"></i>
              </Link>
            </div>
        </nav>
