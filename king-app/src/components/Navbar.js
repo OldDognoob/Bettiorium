ec106e8b02864a433cebae2849fbfd685f292806
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-09-30 20:07:24 +0300

    create-menu-button

--- king-app/src/components/Navbar.js
+++ king-app/src/components/Navbar.js
@@ -10,6 +10,7 @@ function Navbar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   const [theme, toggleTheme] = useDarkMode("light");
 
@@ -26,6 +27,40 @@ function Navbar() {
             <div className="menu-icon" onClick={handleClick}>
               <i className={click ? "fas fa-times" : "fas fa-bars"} />
             </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/about-us"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  About Us
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/videos"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Videos
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/sign-up"
+                  className="nav-links-mobile"
+                  onClick={closeMobileMenu}
+                >
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
           </div>
         </nav>
       </ThemeProvider>
