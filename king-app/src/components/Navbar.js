c8da36ae5538bea9d44d8e177600565c3495e36a
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-09-30 19:32:18 +0300

    dark-functionality

--- king-app/src/components/Navbar.js
+++ king-app/src/components/Navbar.js
@@ -1,25 +1,36 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../theme";
+import { GlobalStyles } from "../global";
+import Toggle from "../Toggle";
+import { useDarkMode } from "../useDarkMode";
 
 function Navbar() {
-    const[click, setClick]= useState(false);
+  const [click, setClick] = useState(false);
 
-    const handleClick = () => setClick(!click);
+  const handleClick = () => setClick(!click);
 
-    return (
-       <>
-       <nav className="navbar">
-           <div className="navbar-container">
-             <Link to="/"className="navbar-logo">
-                 BETTIORIUM <i className="fas fa-crown"></i>
-             </Link>
-             <div className='menu-icon' onClick={handleClick}>
-                 <i className={click ? 'fas fa-times': 'fas fa-bars'}/>
-             </div>
-           </div>
-       </nav>
-       </>
-    )
+  const [theme, toggleTheme] = useDarkMode("light");
+
+  return (
+    <>
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <GlobalStyles />
+        <Toggle onToggle={toggleTheme}>Toggle theme</Toggle>
+        <nav className="navbar">
+          <div className="navbar-container">
+            <Link to="/" className="navbar-logo">
+              BETTIORIUM <i className="fas fa-crown"></i>
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              <i className={click ? "fas fa-times" : "fas fa-bars"} />
+            </div>
+          </div>
+        </nav>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
