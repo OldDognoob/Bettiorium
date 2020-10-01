88f0bb05b2e249ec44747516718246c0a7976c5d
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-01 15:48:00 +0300

    corrections

--- king-app/src/App.js
+++ king-app/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from './components/pages/Home';
-import AboutUs from './components/pages/AboutUs';
-import Articles from './components/pages/Articles';
-import Videos from './components/pages/Videos';
-import SignUp from './components/pages/SignUp';
+// import Home from './components/pages/Home';
+// import AboutUs from './components/pages/AboutUs';
+// import Articles from './components/pages/Articles';
+// import Videos from './components/pages/Videos';
+// import SignUp from './components/pages/SignUp';
 import './App.css';
 
 function App() {
@@ -14,11 +14,11 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home} />
+          {/* <Route path="/" exact component={Home} />
           <Route path='/about-us' component={AboutUs}/>
           <Route path='/articles' component={Articles}/>
           <Route path='/videos' component={Videos}/>
-          {/* <Route path='/sign-up' component={SignUp}/> */}
+          <Route path='/sign-up' component={SignUp}/> */}
         </Switch>
       </Router>
     </>
--- king-app/src/components/HeroSection.js
+++ king-app/src/components/HeroSection.js
@@ -1,29 +1,37 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './HeroSection.css';
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import ReactPlayer from "react-player";
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-      <video src='' autoPlay loop muted />
+    <div className="hero-container">
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=FiXCxfWWwPo"
+        controls
+        playbackRate={2}
+        width="896px"
+        height="504px"
+      />
+
       <h1>KINGDOM AWAITS YOU</h1>
       <p>What are you waiting for?</p>
-      <div className='hero-btns'>
+      <div className="hero-btns">
         <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
         >
           ROCK THE WORLD!
         </Button>
         <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+          onClick={console.log("hey")}
         >
-          WATCH TRAILER <i className='far fa-play-circle' />
+          WATCH TRAILER <i className="far fa-play-circle" />
         </Button>
       </div>
     </div>
