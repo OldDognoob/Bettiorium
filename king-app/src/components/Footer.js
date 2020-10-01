af5dc54941956899ae7125190233dbf12c131d94
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-01 19:37:15 +0300

    wip-project

---
+++ king-app/src/components/Footer.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import './Footer.css';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+          Join our KingDom if you Dare?
+        </p>
+        <p className='footer-subscription-text'>
+          You can be our royal citizen if you wish for any time!.
+        </p>
+        <div className='input-areas'>
+          <form>
+            <input
+              className='footer-input'
+              name='email'
+              type='email'
+              placeholder='Your Email'
+            />
+            <Button buttonStyle='btn--outline'>Royal Subscription</Button>
+          </form>
+        </div>
+      </section>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>KingDom Careers</Link>
+            <Link to='/'>Foreign Investors</Link>
+            <Link to='/'>Royal Terms of Service</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Royal Contact</Link>
+            <Link to='/'>Knight Support</Link>
+            <Link to='/'>Dark KingDom Destinations</Link>
+            <Link to='/'>Faith Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>KingDom Ambassadors</Link>
+            <Link to='/'>Royal Secret Service</Link>
+            <Link to='/'>Bettiorium Influencer</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/https://www.facebook.com/Bettiorium-2212372672383230'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
+            <Link to='/' className='social-logo'>
+              BETTIORIUM
+              <i class='fas fa-crown' />
+            </Link>
+          </div>
+          <small class='website-rights'>BETTIORIUM © 2020</small>
+          <div class='social-icons'>
+            <Link
+              class='social-icon-link facebook'
+              to='/https://www.facebook.com/Bettiorium-2212372672383230'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i class='fab fa-facebook-f' />
+            </Link>
+            <Link
+              class='social-icon-link instagram'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <i class='fab fa-instagram' />
+            </Link>
+            <Link
+              class='social-icon-link youtube'
+              to='/'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <i class='fab fa-youtube' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <i class='fab fa-twitter' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i class='fab fa-linkedin' />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
