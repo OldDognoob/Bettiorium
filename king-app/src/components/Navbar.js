b83b77d7a6074a86ed62e52998596655e61d8323
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-01 12:36:01 +0300

    correction-project

--- king-app/src/components/Navbar.js
+++ king-app/src/components/Navbar.js
@@ -1,83 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "./Button";
 import { Link } from "react-router-dom";
-import { Button } from './Button';
-import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "../theme";
-import { GlobalStyles } from "../global";
-import Toggle from "../Toggle";
-import { useDarkMode } from "../useDarkMode";
-import './Navbar.css';
+import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton]= useState(true);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if(window.innerWidth <=960){
-      setButton(false)
-    }else{
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
       setButton(true);
     }
   };
 
-  window.addEventListener('resize', showButton);
+  useEffect(() => {
+    showButton();
+  }, []);
 
-  const [theme, toggleTheme] = useDarkMode("light");
+  window.addEventListener("resize", showButton);
 
   return (
     <>
-      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-        <GlobalStyles />
-        <Toggle onToggle={toggleTheme}>Toggle theme</Toggle>
-        <nav className="navbar">
-          <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-              BETTIORIUM <i className="fas fa-crown"></i>
-            </Link>
-            <div className="menu-icon" onClick={handleClick}>
-              <i className={click ? "fas fa-times" : "fas fa-bars"} />
-            </div>
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
-              <li className="nav-item">
-                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/about-us"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  About Us
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/videos"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  Videos
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/sign-up"
-                  className="nav-links-mobile"
-                  onClick={closeMobileMenu}
-                >
-                  Sign Up
-                </Link>
-              </li>
-            </ul>
-            {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            TRVL
+            <i class="fab fa-typo3" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
-        </nav>
-      </ThemeProvider>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/about-us"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Articles
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link
+                to="/videos"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Videos
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/articles"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+        </div>
+      </nav>
     </>
   );
 }
