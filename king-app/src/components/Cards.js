2b4a8e4115d0b93157837d7547140e75c518fa92
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-04 14:44:18 +0300

    wip-dark-functionality-not-working

--- king-app/src/components/Cards.js
+++ king-app/src/components/Cards.js
@@ -11,13 +11,13 @@ function Cards() {
           <ul className="cards__items">
             <CardItem
               src="images/Betty.jpg"
-              text="Staying Alive"
+              text="A normal person during the day a medieval queen during the night!"
               label="Queen of Bettiorium"
               path="/about-us"
             />
             <CardItem
               src="images/Nickos.jpg"
-              text="Big in Japan"
+              text="LionHeart Philosopher ready to show your destiny to the outer world! "
               label="Philosopher"
               path="/about-us"
             />
@@ -25,33 +25,33 @@ function Cards() {
           <ul className="cards__items">
             <CardItem
               src="images/Mosxidis.jpg"
-              text="Hot Stuf"
+              text="There is no doubt he might be crazy but at least it keep it interesting!"
               label="Crazy"
               path="/about-us"
             />
             <CardItem
               src="images/GeorgeGiannis.jpg"
-              text="When the Going Gets Tough, the Tough Get Going"
+              text="They are lunatics by nature and lunatics dont need training-they just are!"
               label="Lunatics"
               path="/About-us"
             />
             <CardItem
               src="images/Rigas.jpg"
-              text="Neon Towers"
+              text="May the forces of evil become confused on the way to your house!"
               label="Mentor"
               path="/About-us"
             />
             <CardItem
               src="images/George.jpg"
-              text="Touch Me"
+              text="My mind is like my web browser 19 tabs are open, 3 are frozen and I have no idea where the music is coming from!"
               label="Adventure"
               path="/About-us"
             />
             <CardItem
               src="images/Dimos.jpg"
-              text="Dance Monkey"
+              text="He is the only person to fix your problem, you dont know you have in a way you dont understand!"
               label="Programmer"
-              path="/sign-up"
+              path="/About-us"
             />
           </ul>
         </div>
