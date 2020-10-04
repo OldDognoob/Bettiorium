3f3371a27976f2f82d0b5f1fa447aae1de5b89c3
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-04 15:50:59 +0300

    wip-project

--- king-app/src/App.js
+++ king-app/src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import AboutUs from "./components/pages/AboutUs";
-import Articles from "./components/pages/Articles";
+import ContactUs from "./components/pages/ContactUs";
 import Videos from "./components/pages/Videos";
 import SignUp from "./components/pages/SignUp";
 import "./App.css";
@@ -16,8 +16,8 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about-us" component={AboutUs} />
-          <Route path="/articles" component={Articles} />
           <Route path="/videos" component={Videos} />
+          <Route path="/contact-us" component={ContactUs} />
           <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
