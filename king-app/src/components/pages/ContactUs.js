1de2c43f5469711d5de7796411344c1ca6a0c8cb
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-04 19:57:06 +0300

    changes

--- king-app/src/components/pages/ContactUs.js
+++ king-app/src/components/pages/ContactUs.js
@@ -2,13 +2,11 @@ import React from "react";
 import Raven from "../../raven.jpg";
 import Footer from "../Footer";
 
-
-
 function ContactUs() {
   return (
     <>
       <div className="hero">
-        <img src={Raven} alt="raven"/>
+        <img src={Raven} alt="raven" />
         <Footer />
       </div>
     </>
