b23b98c5bd694bfdbe3dc6e1158596d3a1365470
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-04 15:48:11 +0300

    linked-abou-us

--- king-app/src/components/pages/AboutUs.js
+++ king-app/src/components/pages/AboutUs.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import Cards from '../Cards';
+import Videos from '../pages/Videos';
 import '../../App.css';
 
-export default function AboutUs() {
-  return <h1 className='aboutUs'>ABOUT US</h1>;
+// export default function AboutUs() {
+//   return <h1 className='aboutUs'>ABOUT US</h1>;
+// }
+
+function AboutUs() {
+  return (
+    <>
+      <Cards />
+    </>
+  );
 }
+
+export default AboutUs;
