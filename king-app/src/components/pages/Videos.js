57c9bbde60f0105a89384e2c18e6d66b9bbb59fa
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-11 21:44:48 +0300

    changes

--- king-app/src/components/pages/Videos.js
+++ king-app/src/components/pages/Videos.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import "../Cards.css";
+import ReactPlayer from "react-player";
+import Green from "../../green.jpg";
 import '../../App.css';
 
-export default function Videos() {
-  return <h1 className='videos'>VIDEOS</h1>;
+function Videos(){
+  return(
+    <>
+    <div className="hero">
+      <img src={Green} alt="green"/>
+    </div>
+    </>
+  )
 }
+export default Videos;
