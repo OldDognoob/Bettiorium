0f6c4c376164ff68df102391abd9ac4f5ef78b33
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-20 16:26:13 +0300

    wip-about-us

--- king-app/src/components/pages/AboutUs.js
+++ king-app/src/components/pages/AboutUs.js
@@ -1,21 +1,16 @@
 import React from "react";
 import Cards from "../Cards";
 import Knight from "../Knight";
-// import Footer from "../Footer";
-// import Videos from "../pages/Videos";
 import Dragon from "./../../dragon.jpg";
 import "../../App.css";
 
-// export default function AboutUs() {
-//   return <h1 className='aboutUs'>ABOUT US</h1>;
-// }
 
 function AboutUs() {
   return (
     <>
       <div className="hero">
-        <img src={Dragon} alt="dragon"/>
-        <Knight/>
+        <img src={Dragon} alt="dragon" />
+        <Knight />
         <Cards />
         {/* <Footer /> */}
       </div>
