4c30c4f3b6dc5741903ffd86b58f2aedb91d9f8f
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-23 19:03:01 +0300

    map-adding-progress

--- king-app/src/components/pages/Home.js
+++ king-app/src/components/pages/Home.js
@@ -3,13 +3,14 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
-
+// import Map from '../Map';
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
+      {/* <Map/> */}
       <Footer />
     </>
   );
